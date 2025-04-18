b42aeb54ca9bf38313439792e2a0953b4423e1d1
Ardasher <misterardasher@gmail.com>
2025-04-18 17:29:33 +0500

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
+import { createRouter } from "@tanstack/react-router";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { routeTree } from "./routeTree.gen.ts";
 import "./styles/tailwind.css";
 import './common/i18n'
-import { App } from "./App";
 
+const router = createRouter({ routeTree });
 
-
+declare module "@tanstack/react-router" {
+	interface Register {
+		// This infers the type of our router and registers it across your entire project
+		router: typeof router;
+	}
+}
 
 
 const rootElement = document.querySelector("#root") as Element;
@@ -14,7 +22,7 @@ if (!rootElement.innerHTML) {
 	root.render(
 		<React.StrictMode>
 			<React.Suspense fallback="loading">
-				<App />
+				<App router={router} />
 			</React.Suspense>
 		</React.StrictMode>
 	);

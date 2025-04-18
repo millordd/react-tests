ac8ca0d997165ed87e28f39d2d45aa6ea7099348
Ardasher <121037431+millordd@users.noreply.github.com>
2025-04-18 16:26:31 +0500

    Revert "feat: update tailwind configuration"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,47 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{tsx,ts}'],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: 'fontAlsHauss',
-      container: {
-        center: true,
-        padding: {
-          DEFAULT: '16px',
-        },
-      },
-      spacing: {
-        topbar: '52px',
-        navbar: '60px',
-      },
-      colors: {
-        primary: 'var(--primary)',
-        system: {
-          error: '#DF2638',
-          notification: '#EE8042',
-          note: 'rgba(255, 181, 0, 0.12)',
-          'icon-color': '#F7F8F9',
-          'error-pale': 'rgba(223, 38, 56, 0.16)',
-          former: '#FFFFFF',
-          'search-bar-bg': 'rgba(170, 170, 177, 0.16)',
-          black: '#222222',
-          overlay: 'rgba(0, 0, 0, 0.5)',
-          grey: '#7a8189',
-          'grey-50': '#9BA1A7',
-        },
-      },
-    },
+    extend: {},
   },
-  corePlugins: {
-    preflight: true,
-  },
-  plugins: [
-    ({ addBase }) => {
-      addBase({
-        html: {
-          fontFamily: 'fontAlsHauss',
-        },
-      });
-    },
-  ],
+  plugins: [],
 };
